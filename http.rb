225bbe64db515cc73bec7a33099276bed15c8fa1
Gingertonic <bethmschofield@gmail.com>
2020-04-16 14:51:09 +0100

    add class structure

--- http.rb
+++ http.rb
@@ -2,9 +2,19 @@ require "httparty"
 require "pry"
 require "launchy"
 
-data = HTTParty.get('https://learn.co/magazine/blog-posts?page=1&page_size=53')
+class API 
+    def self.get_posts
+        data = HTTParty.get('https://learn.co/magazine/blog-posts?page=1&page_size=200')
 
-blog_posts = data["blog_posts"]
+        blog_posts = data["blog_posts"]
+
+        blog_posts.each do |post_data|
+            BlogPost.new(post_data)
+        end 
+    end
+end
+
+
 
 class BlogPost 
     attr_accessor :title, :lesson_title, :author, :excerpt, :url
@@ -20,49 +30,54 @@ class BlogPost
     end 
 
     def self.all 
+        API.get_posts if @@all.empty?
         @@all
     end 
+
+    def self.find_related_to(query)
+        all.find_all{|bp| bp.title.downcase.include?(query)}
+    end
 end 
 
-blog_posts.each do |post_data|
-    BlogPost.new(post_data)
-end 
 
-def list(query)
-    matching = find_related_to(query)
-    matching.each.with_index(1) do |bp, i|
-        puts "#{i}. #{bp.title}"
+class CLI
+
+    def list(query)
+        matching = BlogPost.find_related_to(query)
+        matching.each.with_index(1) do |bp, i|
+            puts "#{i}. #{bp.title}"
+        end 
+        puts "\nChoose one!\n"
+        show_one(matching)
+    end
+
+    def app_loop 
+        puts "\nWhaddya wanna see?\n"
+        user_input = gets.strip
+        if user_input != 'exit'
+            list(user_input)
+            puts "\nHit enter to continue"
+            gets 
+            app_loop
+        end 
+        puts "Ciao"
     end 
-    puts "\nChoose one!\n"
-    show_one(matching)
-end
 
-def find_related_to(query)
-    BlogPost.all.find_all{|bp| bp.title.downcase.include?(query)}
-end
+    def show_one(matches)
+        input = gets.strip.to_i - 1
+        post = matches[input]
+        puts "\n#{post.title}"
+        puts post.author
+        puts post.excerpt 
+        puts "\nType 'open' to see full post in browser\n"
+        input = gets.strip 
+        Launchy.open(post.url) if input == "open"
+    end 
 
-
-def app_loop 
-    puts "\nwhaddya wanne see?\n"
-    user_input = gets.strip
-    if user_input != 'exit'
-        list(user_input)
-        puts "\nHit enter to continue"
-        gets 
+    def run
+        puts "Hello!"
         app_loop
     end 
-    puts "ciao"
 end 
 
-def show_one(matches)
-    input = gets.strip.to_i - 1
-    post = matches[input]
-    puts "\n#{post.title}"
-    puts post.author
-    puts post.excerpt 
-    puts "\nType 'open' to see full post in browser\n"
-    input = gets.strip 
-    Launchy.open(post.url) if input == "open"
-end 
-
-app_loop
+CLI.new.run
